3cf35c6ff7514207202e0b6f63525a1cc2217b9c
Vynex <kushagra100910@gmail.com>
2021-03-22 12:46:06 +0530

    simplified password validation

--- controllers/users.js
+++ controllers/users.js
@@ -6,13 +6,15 @@ export function registerForm(req, res) {
 
 export async function register(req, res) {
 	try {
-		const { email, username, password } = req.body;
-		const user = new User({ email, username, password });
+		const { email, username, password, repeat_password } = req.body;
+		console.log(username);
+		const user = new User({ email, username, password, repeat_password });
 		await user.save();
 
 		req.flash('success', 'welcome');
 		res.redirect('/');
 	} catch (err) {
+		console.log(err);
 		req.flash('error', err.message);
 		res.redirect('/register');
 	}
